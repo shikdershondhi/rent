ed8721a0d7fe1c825aa5cac113ca0a8c2bcbd9f3
shikdershondhi <ahateshamulislam@gmail.com>
2025-08-16 17:24:25 +0600

    feat: Add theme persistence and update RentApp to support theme loading

--- lib/app.dart
+++ lib/app.dart
@@ -2,8 +2,19 @@ import 'package:flutter/material.dart';
 import 'core/theme.dart';
 import 'features/home/home_screen.dart';
 
-class RentApp extends StatelessWidget {
+class RentApp extends StatefulWidget {
   const RentApp({super.key});
+
+  @override
+  State<RentApp> createState() => _RentAppState();
+}
+
+class _RentAppState extends State<RentApp> {
+  @override
+  void initState() {
+    super.initState();
+    ThemePersistence.loadThemeMode();
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final ValueNotifier<ThemeMode> themeNotifier = ValueNotifier(ThemeMode.light);
+
+class ThemePersistence {
+  static const _key = 'theme_mode';
+
+  static Future<void> saveThemeMode(ThemeMode mode) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_key, mode == ThemeMode.dark ? 'dark' : 'light');
+  }
+
+  static Future<void> loadThemeMode() async {
+    final prefs = await SharedPreferences.getInstance();
+    final value = prefs.getString(_key);
+    if (value == 'dark') {
+      themeNotifier.value = ThemeMode.dark;
+    } else {
+      themeNotifier.value = ThemeMode.light;
+    }
+  }
+}
 
 class AppTheme {
   static final ThemeData lightTheme = ThemeData(
